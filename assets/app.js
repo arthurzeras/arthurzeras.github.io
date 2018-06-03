cb23c94ecea7142325867cc6934cc64927431d98
arthurzeras <arthurolvmorais@gmail.com>
2018-06-03 15:52:27 -0300

    Add modals

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,19 @@
+Vue.component('my-modal', {
+  template: `
+  <div class="my-modal">
+    <button class="btn close-button" @click="$emit('close')">
+      <i class="zmdi zmdi-close zmdi-hc-2x"></i>
+    </button>
+    <slot></slot>
+  </div>
+  `
+})
+
 new Vue({
   el: '#app',
-  data () {
-    return {
-      buttons: false
-    }
+  data: {
+    pageState: 'home',
+    buttons: false
   },
   mounted () {
     setTimeout(() => {
