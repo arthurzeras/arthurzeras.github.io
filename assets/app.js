19c31cf2f5dea82320666d3ce95234ee69f16ead
arthurzeras <arthurolvmorais@gmail.com>
2018-06-03 15:07:48 -0300

    Add transitions

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,12 @@ new Vue({
   el: '#app',
   data () {
     return {
-      
+      buttons: false
     }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.buttons = true
+    }, 1000)
   }
 })
