deaeb7b5b4026e56f79b3bdc7450af44966d6ae1
arthurzeras <arthurolvmorais@gmail.com>
2018-06-03 21:13:35 -0300

    Add knowledges

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,31 @@ new Vue({
   el: '#app',
   data: {
     pageState: 'home',
-    buttons: false
+    buttons: false,
+    knowledges: [
+      {icon: 'devicon-javascript-plain', name: 'Javascript'},
+      {icon: 'devicon-vuejs-plain', name: 'Vue.js'},
+      {icon: 'devicon-php-plain', name: 'PHP'},
+      {icon: 'devicon-laravel-plain', name: 'Laravel'},
+      {icon: 'devicon-sass-original', name: 'SASS'},
+      {icon: 'devicon-html5-plain', name: 'HTML5'},
+      {icon: 'devicon-nodejs-plain', name: 'Node JS'},
+      {icon: 'devicon-bootstrap-plain', name: 'Bootstrap'},
+      {icon: 'devicon-css3-plain', name: 'CSS3'},
+      {icon: 'devicon-express-original', name: 'Express'},
+      {icon: 'devicon-git-plain', name: 'Git'},
+      {icon: 'devicon-github-plain', name: 'Github'},
+      {icon: 'devicon-bitbucket-plain', name: 'Bitbucket'},
+      {icon: 'devicon-mysql-plain', name: 'MySQL'},
+      {icon: 'devicon-apple-original', name: 'MacOS'},
+      {icon: 'devicon-windows8-plain', name: 'Windows'},
+      {icon: 'devicon-linux-plain', name: 'Linux'},
+      {icon: 'devicon-photoshop-plain', name: 'Photoshop'},
+      {icon: 'devicon-slack-plain', name: 'Slack'},
+      {icon: 'devicon-webpack-plain', name: 'Webpack'},
+      {icon: 'devicon-phpstorm-plain', name: 'PHP Storm'},
+      {icon: 'devicon-webstorm-plain', name: 'WebStorm'}
+    ]
   },
   mounted () {
     setTimeout(() => {
